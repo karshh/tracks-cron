c187c2770fbdfb5434fe5e2824a1595fa494f2af
karshh <karsharma24@gmail.com>
2020-03-27 22:18:30 -0600

    Init commit.

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import * as express from 'express'
+import { Application } from 'express'
+import * as mongoose from 'mongoose'
+
+class App {
+    public app: Application
+    public port: number
+
+    constructor(appInit: { port: number; middleWares: any; controllers: any; }) {
+        this.app = express()
+        this.port = appInit.port
+
+        this.middlewares(appInit.middleWares)
+        this.routes(appInit.controllers)
+        var connectionURL = 'mongodb://127.0.0.1/torn_data';
+        mongoose.connect(connectionURL, { useNewUrlParser: true }).then(
+          () => console.log(`Succesfully connected to ${connectionURL}`),
+          (err) => console.error("Connection error", err));
+
+    }
+
+    private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
+        middleWares.forEach(middleWare => {
+            this.app.use(middleWare)
+        })
+    }
+
+    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+}
+
+export default App
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import App from './app'
+import * as bodyParser from 'body-parser'
+
+
+const app = new App({
+    port: 5000,
+    controllers: [
+    ],
+    middleWares: [
+        bodyParser.json(),
+        bodyParser.urlencoded({ extended: true }),
+    ]
+})
+
+app.listen()
